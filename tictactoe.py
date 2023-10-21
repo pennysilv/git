4478d816000dca51757926f80ad59f5735cbdd33
penny_silv <pennysilv@gmail.com>
2023-10-21 18:03:38 +0500

    добавила ничью

--- tictactoe.py
+++ tictactoe.py
@@ -33,59 +33,59 @@ def switch_player():
     else:
         current_player = X
 
-def change_map(row, column):
+def change_map(map, row, column):
     global current_player
-    global map
-
     map[row - 1][column - 1] = current_player
 
+def is_filled(map, row, column):
+    return map[row - 1][column - 1] != Empty
 
-def is_filled(row, column):
-    global map
-    
-    # TODO: Это если что, то же самое
-    # return map[row][column] != Empty
-    if map[row - 1][column - 1] != Empty:
+def is_winner(map, player):
+    if map[0][0] == map[0][1] == map[0][2] == player:
         return True
-    else:
-        return False
-    
-def is_winner():
-    global map
-    global current_player
-
-    if map[0][0] == map[0][1] == map[0][2] == current_player:
+    elif map[1][0] == map[1][1] == map[1][2] == player:
         return True
-    elif map[1][0] == map[1][1] == map[1][2] == current_player:
+    elif map[2][0] == map[2][1] == map[2][2] == player:
         return True
-    elif map[2][0] == map[2][1] == map[2][2] == current_player:
+    elif map[0][0] == map[1][0] == map[2][0] == player:
         return True
-    elif map[0][0] == map[1][0] == map[2][0] == current_player:
+    elif map[0][1] == map[1][1] == map[2][1] == player:
         return True
-    elif map[0][1] == map[1][1] == map[2][1] == current_player:
+    elif map[0][2] == map[1][2] == map[2][2] == player:
         return True
-    elif map[0][2] == map[1][2] == map[2][2] == current_player:
+    elif map[2][0] == map[1][1] == map[0][2] == player:
         return True
-    elif map[2][0] == map[1][1] == map[0][2] == current_player:
-        return True
-    elif map[0][0] == map[1][1] == map[2][2] == current_player:
+    elif map[0][0] == map[1][1] == map[2][2] == player:
         return True
 
+def is_full(map):
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == Empty:
+                return False
+    return True
 
+def is_tie(map):
+    return not is_winner(map, X) and not is_winner(map, O) and is_full(map)
 
 while True:
     print_map(map)
     row, column = get_player_turn()
-    while is_filled(row, column):
+    while is_filled(map, row, column):
         print('Клетка уже заполнена')
         row, column = get_player_turn()
 
-    change_map(row, column)
-    if is_winner():
+    change_map(map, row, column)
+    if is_winner(map, current_player):
         print_map(map)
         if current_player == X:
             print('Победили крестики!')
         else:
             print('Победили нолики!')
         break
+    elif is_tie(map):
+        print_map(map)
+        print("Ничья!")
+        break
+
     switch_player()
